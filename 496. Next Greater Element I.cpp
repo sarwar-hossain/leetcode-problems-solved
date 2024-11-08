4f3877586ebaf2291681169b4e0c58e1a02d679d
sarwar-hossain <sarwar163264@gmail.com>
2024-11-08 11:36:37 +0530

    Add files via upload

---
+++ 496. Next Greater Element I.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+//1
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       unordered_map<int, int>um;
+       stack<int>s;
+
+       for(int num : nums2){
+           while(!s.empty() && num>s.top()){
+              um[s.top()]=num;
+              s.pop();
+           }
+           s.push(num);
+       }
+
+       while(!s.empty()){
+          um[s.top()]=-1;
+          s.pop();
+       }
+       
+       vector<int>r;
+
+       for(int num : nums1){
+        r.push_back(um[num]);
+       }
+
+       return r;
+
+    }
+};
