3144ee19028fb6c56291c6abf60cd6fa9256d4d6
sarwar-hossain <sarwar163264@gmail.com>
2024-11-27 11:46:58 +0530

    Add files via upload

---
+++ 572. Subtree of Another Tree.cpp
@@ -0,0 +1,38 @@
+//1
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isSametree(TreeNode* s,TreeNode* t){
+    if(!s && !t) return true;
+    if(!s || !t) return false;
+    if(s->val!=t->val) return false;
+
+    return isSametree(s->left, t->left) && isSametree(s->right, t->right);
+   }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root){
+            return false;
+        }
+
+        if(isSametree(root, subRoot)){
+            return true;
+        }
+
+        return (isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot));
+    }
+};
